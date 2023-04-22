fd89225d63ae97f1bb6dcd973dc6aa508fe69cf5
Jake Johnston <jakejohnston618@gmail.com>
2023-04-22 13:35:00 -0500

    finished kanye and ron

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using APIsAndJSON;
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool cont = true;
+
+            do
+            {
+                Quotes.KanyeQuote();
+                Quotes.RonQuote();
+
+                Console.WriteLine("Continue? Y or N?");
+                var response = Console.ReadLine().ToLower();
+
+                cont = (response == "n") ? false : true;
+            }while (cont);
+
+
+            
         }
-    }
+    } 
 }
